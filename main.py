5d4315a9b244e11230ea3b435ae7078d55e84389
BrinonBat <baptiste@brinon.eu>
2021-11-17 08:34:22 +0100

    maj tests

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import Graphe
 
 test=Graphe.Graphe("DSJC500.1")
+
+for elem in range(len(test.mat_adj)):
+    if test.aUneCouleurValide(elem)!=True : print("mal conçu !")
+
 print(test)
 
-coloration=[2,1,2]
-print("coloration test "+str(Graphe.evalueColoration(coloration)))
+
+
+print(test.nb_couleurs)
+#coloration=[2,1,2]
+print("coloration test "+str(Graphe.evalueColoration(test.couleurs)))
