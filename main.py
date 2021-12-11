ef0333bdb94b1ffe51fd9ccf0f4caf205ad048e9
BrinonBat <baptiste@brinon.eu>
2021-12-11 21:33:28 +0100

    ajout d'un chronomètre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import Graphe
 import metaheuristiques
-
+import time
 
 
 
@@ -10,7 +10,9 @@ for li in s:
     seeds=li.split()
     seed=int(seeds[0])
 
+#premiere coloration
 test=Graphe.Graphe("DSJC500.1",seed)
+
 est_valide=True
 for elem in range(len(test.mat_adj)):
     if test.aUneCouleurValide(elem)!=True : est_valide=False
@@ -20,16 +22,16 @@ print(test)
 
 
 print(test.nb_couleurs)
-#coloration=[2,1,2]
 print("coloration test "+str(Graphe.evalueColoration(test.couleurs)))
 
-#test.changeCouleur(0,test.set_couleurs[0][0])
-#print(test)
-#print("modification test "+str(Graphe.evalueColoration(test.couleurs)))
-
+duree=time.time()
+#lancement de l'algo
 taille_liste=2
-metaheuristiques.tabou(test,taille_liste)
+metaheuristiques.tabu_nouv(test,taille_liste)
+end=time.time()
+duree=(end-duree)
 
 print(test)
 print(test.nb_couleurs)
 print(" est valide ? "+str(test.estValide()))
+print('durée totale :'+str(duree))
